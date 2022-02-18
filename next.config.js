a84baa5a926c2127855a80a279006f99a12c5330
szeanx <sean@relampagos.org>
2022-02-18 14:49:09 +0800

    edited dist dir to build

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 module.exports = {
-  reactStrictMode: true,
-  images: {
-    domains: ["images.unsplash.com"],
-  },
+    reactStrictMode: true,
+    images: {
+        domains: ["images.unsplash.com"],
+    },
+    distDir: "build",
 };
