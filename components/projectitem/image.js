2e5504e51657c39e44c43516820d110af1ccaef7
szeanx <sean@relampagos.org>
2022-05-13 10:42:44 +0800

    accepted current change

--- components/projectitem/image.js
+++ components/projectitem/image.js
@@ -1,15 +1,6 @@
 import styled from "@emotion/styled";
 
 const ProjectImageStyled = styled.img`
-<<<<<<< HEAD
-  position: fixed;
-  left: 60vw;
-  width: 300px;
-  object-fit: cover;
-  top: 30vh;
-  pointer-events: none;
-  transition: transform 1s cubic-bezier(0.165, 0.84, 0.44, 1);
-=======
     position: fixed;
     left: 60vw;
     width: 300px;
@@ -17,7 +8,6 @@ const ProjectImageStyled = styled.img`
     top: 30vh;
     pointer-events: none;
     transition: transform 1s cubic-bezier(0.165, 0.84, 0.44, 1);
->>>>>>> a40aeaec618339a0332e399d489f9891f3f265a0
 `;
 
 const ProjectImage = ({ url, opacity, parallaxPos, scale, rotationPos }) => {
